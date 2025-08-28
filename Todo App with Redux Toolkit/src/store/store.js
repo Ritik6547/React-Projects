b3e04749486c90c2bf426986ad6f45ed7c5f7558
Ritik <alphaop6547@gmail.com>
2025-08-29 03:30:16 +0530

    Implement localStorage to persist todos

--- Todo App with Redux Toolkit/src/store/store.js
+++ Todo App with Redux Toolkit/src/store/store.js
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "./slices/todosSlice.js";
 import todosFormReducer from "./slices/todosFormSlice.js";
+import { loadState, saveState } from "../utils/localStorage.js";
+
+const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: {
     todos: todosReducer,
     todosForm: todosFormReducer,
   },
+  preloadedState,
+});
+
+store.subscribe(() => {
+  saveState({
+    todos: store.getState().todos,
+  });
 });
